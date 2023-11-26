177efbc5302ff12708f75e3702299310ee4ba727
JohnKalvin0425 <148409956+JohnKalvin0425@users.noreply.github.com>
2023-11-26 13:32:46 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}", "./node_modules/react-tailwindcss-datepicker/dist/index.esm.js", 
+    "./node_modules/tw-elements-react/dist/js/**/*.js"
+
+   
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    
+    require('flowbite/plugin'),
+    require('daisyui'),
+    
+],
+
+}
+    
